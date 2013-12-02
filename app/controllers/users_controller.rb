d50848e8413330d094293e5c22df973500e6c5b2
Sean McAuliffe <smcauliffe2@gmail.com>
2013-12-02 08:59:25 -0500

    adding users and validating unique name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
 
   def index
-
+    @users = User.all
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
-    render text: params[:user].inspect
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 
 end
